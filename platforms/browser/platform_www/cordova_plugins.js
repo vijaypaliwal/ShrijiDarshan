c18450cec3f71d10015618a3c36845d5b9a86f7f
vijaypaliwal <vijay.paliwal@hotmail.com>
2017-11-22 10:56:12 +0530

    first addintion
    first add code for push notification

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "id": "phonegap-plugin-push.PushNotification",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-push/www/browser/push.js",
+        "id": "phonegap-plugin-push.BrowserPush",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "phonegap-plugin-push": "1.8.4"
+}
+// BOTTOM OF METADATA
+});
