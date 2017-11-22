021d15ae30be1534bcc44df81c1d48f0c7323f72
vijaypaliwal <vijay.paliwal@hotmail.com>
2017-11-22 16:21:37 +0530

    sdfs
    sdfsdf

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-fcm.FCMPlugin",
+    "file": "plugins/cordova-plugin-fcm/www/FCMPlugin.js",
+    "pluginId": "cordova-plugin-fcm",
+    "clobbers": [
+      "FCMPlugin"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-fcm": "2.1.2"
+};
+// BOTTOM OF METADATA
+});
